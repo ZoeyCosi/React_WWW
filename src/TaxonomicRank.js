0f0dd88e60f3f7176d3fa18aa467f81b92319f38
Joseph <ishojoseph@gmail.com>
2022-05-22 19:59:44 -0400

    Updatev2

--- src/TaxonomicRank.js
+++ src/TaxonomicRank.js
@@ -9,6 +9,9 @@ export class TaxonomicRank extends Component{
         this.state={
             tranks:[],
             isLoaded: false,
+            modalTitle:"",
+
+
         }
     }
 
@@ -19,6 +22,8 @@ export class TaxonomicRank extends Component{
             this.setState({
                 tranks:data,
                 isLoaded: true,
+                Name:"",
+                Id:0
             })
         });
     }
@@ -27,9 +32,35 @@ export class TaxonomicRank extends Component{
         this.refreshList();
     }
 
+    changeName =(e)=>{
+        this.setState({Name:e.target.value});
+    }
+
+    addClick(){
+        this.setState({
+            modalTitle:"Add Domain",
+            Id:0,
+            Name:""
+        });
+    }
+
+    editClick(trank){
+        this.setState({
+            modalTitle:"Edit Domain",
+            Id:trank.Id,
+            Name:trank.Name
+        });
+    }
+
     render(){
 
-        var {isLoaded, tranks} = this.state;
+        var {
+            isLoaded,
+            modalTitle, 
+            tranks,
+            Name,
+            Id
+        } = this.state;
 
         if (!isLoaded)
         {
@@ -39,12 +70,20 @@ export class TaxonomicRank extends Component{
         {
             return(
                 <div>
+                    <button type="button" id="addButton"
+                    className="btn btn-primary m-2 float-end"
+                    data-bs-toggle="modal"
+                    data-bs-target="exampleModal"
+                    onClick={()=>this.addClick()}>
+                        Add Domain
+                    </button>
                     <table className="table container">
                         <thead>
                             <tr>
                                 <th>Name</th>
                                 <th>Description</th>
                                 <th>photoPath</th>
+                                <th>Options</th>
                             </tr>
                         </thead>
                         <tbody id="test">
@@ -53,10 +92,55 @@ export class TaxonomicRank extends Component{
                                 <td>{trank.Name}</td>
                                 <td>{trank.Description}</td>
                                 <td>{trank.PhotoURL}</td>
+                                <td>
+                                    <button type="button" className="btn btn-warning"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="exampleModal"
+                                    onClick={()=>this.editClick(trank)}>
+                                        Edit
+                                    </button>
+                                    <button type="button" className="btn btn-danger">
+                                        Delete
+                                    </button>
+                                </td>
                             </tr>
                             )}
                         </tbody>
                     </table>
+                    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-hidden="true">
+                    <div className="modal-dialog modal-lg modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-title">
+                            <h5 className="modal-title">{modalTitle}</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                            </button>
+                        </div>
+
+                        <div className="modal-body">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">Name</span>
+                                <input type="text" className="form-control"
+                                value={Name}
+                                onChange={this.changeName}/>
+                            </div>
+
+                            {Id==0?
+                            <button type="button"
+                            className="btn btn-primary float-start"
+                            >Create</button>
+                            :null}
+                            
+                            {Id!=0?
+                            <button type="button"
+                            className="btn btn-primary float-start"
+                            >Update</button>
+                            :null}
+
+                        </div>
+
+                    </div>
+                    </div>
+                    </div>
                 </div>
             )
         }     
