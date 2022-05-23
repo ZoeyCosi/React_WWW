ce78e9091327665ad63867b468ed495b668af300
Joseph <ishojoseph@gmail.com>
2022-05-22 20:25:12 -0400

    rabbit

---
+++ src/Rabbit.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+export class Rabbit extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This is a rabbit</h3>
+            </div>
+        )
+    }
+}
